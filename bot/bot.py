9957691157d673a5598be98a71f7808db2823d7b
gerstudent <gkolcherin@gmail.com>
2023-08-10 23:17:16 +0300

    Add logger and routers

--- bot/bot.py
+++ bot/bot.py
@@ -1,16 +1,28 @@
 import asyncio
+import logging
 
 from aiogram import Bot, Dispatcher
 
 from config_reader import config
 from handlers import base, list_links, add_link
 
+logger = logging.getLogger(__name__)
 
 # Запуск бота
 async def main():
-    bot = Bot(token=config.bot_token.get_secret_value(), parse_mode='MarkdownV2')
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s')
+    logger.info('Starting bot')
+
+    bot = Bot(token=config.bot_token.get_secret_value(),
+              parse_mode='MarkdownV2')
     dp = Dispatcher()
-    dp.include_routers(base.router, list_links.router, add_link.router)
+
+    dp.include_router(base.router)
+    dp.include_router(add_link.router)
+    dp.include_router(list_links.router)
 
     # Запускаем бота и пропускаем все накопленные входящие
     # Да, этот метод можно вызвать даже если у вас поллинг
